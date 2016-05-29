ef54a176e415ca96b46feb42731b1b69ad6bd317
aquinofb <aquinofb@gmail.com>
2016-05-29 08:08:26 -0300

    Refac views

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,4 +1,3 @@
-# Change these
 server '23.20.100.45', port: 22, roles: [:web, :app, :db], primary: true
 
 set :repo_url,        'git@github.com:aquinofb/open_weather.git'
@@ -7,7 +6,6 @@ set :user,            'ubuntu'
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
 
-# Don't change these unless you know what you're doing
 set :pty,             true
 set :use_sudo,        false
 set :stage,           :production
@@ -21,7 +19,7 @@ set :puma_error_log,  "#{release_path}/log/puma.access.log"
 set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
-set :puma_init_active_record, true  # Change to false when not using ActiveRecord
+set :puma_init_active_record, true
 
 set :ssh_options, {
   keys: ["#{Dir.pwd}/open-weather.pem"],
@@ -33,17 +31,6 @@ set :default_env, {
   'SECRET_KEY_BASE' => ENV['SECRET_KEY_BASE'],
   'OPENWEATHER_KEY' => ENV['OPENWEATHER_KEY'],
 }
-
-## Defaults:
-# set :scm,           :git
-# set :branch,        :master
-# set :format,        :pretty
-# set :log_level,     :debug
-# set :keep_releases, 5
-
-## Linked Files & Directories (Default None):
-# set :linked_files, %w{config/database.yml}
-# set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 namespace :puma do
   desc 'Create Directories for Puma Pids and Socket'
@@ -89,7 +76,3 @@ namespace :deploy do
   after  :finishing,    :cleanup
   after  :finishing,    :restart
 end
-
-# ps aux | grep puma    # Get puma pid
-# kill -s SIGUSR2 pid   # Restart puma
-# kill -s SIGTERM pid   # Stop puma
