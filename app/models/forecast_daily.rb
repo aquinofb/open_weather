136ae84b22450216e2579b1ec047c210c34f4a61
aquinofb <aquinofb@gmail.com>
2016-05-28 21:43:05 -0300

    [refac] delegating method calls

--- app/models/forecast_daily.rb
+++ app/models/forecast_daily.rb
@@ -1,3 +1,6 @@
 class ForecastDaily < Base
   attr_accessor :id, :date, :pressure, :humidity, :speed, :degrees, :clouds, :temperature, :weather
+
+  delegate :icon, :description, to: :weather
+  delegate :min, :max, to: :temperature
 end
